7a227a5c59378a69cd445394168fdec215134f44
iadokonat <iadokonat0106@gmail.com>
2024-01-20 13:27:27 +0900

    import fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Rubik_Burned } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const rubikBurned = Rubik_Burned({ subsets: ['latin'], weight: '400' });
